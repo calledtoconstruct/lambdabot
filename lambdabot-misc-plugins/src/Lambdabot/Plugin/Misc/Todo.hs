358356c1e7d2db2c3fa063a9d118e1f6491a1694
Joseph Woolley <joe@calledtoconstruct.net>
2019-03-25 01:50:46 -0400

    Updating todo plugin to update completion status; refactoring several methods.

--- lambdabot-misc-plugins/src/Lambdabot/Plugin/Misc/Todo.hs
+++ lambdabot-misc-plugins/src/Lambdabot/Plugin/Misc/Todo.hs
@@ -1,8 +1,7 @@
 
+{-# OPTIONS_GHC -fno-warn-overlapping-patterns -Wmissing-signatures #-}
 {-# LANGUAGE PatternGuards #-}
 
--- | A todo list
---
 -- (c) 2005 Samuel Bronson
 
 module Lambdabot.Plugin.Misc.Todo (
@@ -12,11 +11,14 @@ module Lambdabot.Plugin.Misc.Todo (
 import Lambdabot.Compat.PackedNick
 import Lambdabot.Plugin
 import Control.Monad
+import Data.Maybe
 import qualified Data.ByteString.Char8 as P
 
--- A list of key/elem pairs with an ordering determined by its position in the list
-type TodoState = [ AssociativeCompletionState ]
+type TodoState = [AssociativeCompletionState]
 type Todo = ModuleT TodoState LB
+type IndexedEntry = (Int, AssociativeCompletionState)
+type TodoStateTransform = ([IndexedEntry] -> [IndexedEntry])
+type TodoStateTransformResult = (Maybe TodoState, String)
 
 todoPlugin :: Module TodoState
 todoPlugin = newModule {
@@ -33,48 +35,68 @@ todoPlugin = newModule {
       process = addTodo,
       aliases = ["todo-add"]
     },
-    -- (command "update-todo") {
-    --   help = say "update-todo <complete>. Mark a todo entry as complete (True) or incomplete (False)",
-    --   process = updateTodo,
-    --   aliases = ["mark-todo"]
-    -- },
+    (command "update-todo") {
+      help = say "update-todo <index> <complete>. Mark a todo entry as complete (True) or incomplete (False)",
+      process = updateTodo,
+      aliases = ["mark-todo"]
+    },
     (command "delete-todo") {
       privileged = True,
-      help = say "todo-delete <n>. Delete a todo entry (for admins)",
+      help = say "todo-delete <index>. Delete a todo entry (for admins)",
       process = delTodo,
       aliases = ["todo-delete", "remove-todo", "todo-remove"]
     }
   ]
 }
 
--- | Print todo list
 getTodo :: String -> Cmd Todo ()
 getTodo [] = readMS >>= sayTodo
 getTodo _  = say "?todo has no args, try ?add-todo or ?list todo"
 
--- | Pretty print todo list
-sayTodo :: [AssociativeCompletionState] -> Cmd Todo ()
+sayTodo :: TodoState -> Cmd Todo ()
 sayTodo [] = say "Nothing to do!"
-sayTodo todoList = say . unlines =<< zipWithM fmtTodoItem ([0..] :: [Int]) todoList
+sayTodo todoList = do
+  (mapM_ say) =<< zipWithM fmtTodoItem ([1..] :: [Int]) todoList
   where fmtTodoItem n (idea, nick_, state) = do
           nick <- showNick (unpackNick nick_)
-          return $ concat $ [ show n, ". ", nick, ": ", P.unpack idea, if state then " is complete." else " is not complete." ]
+          return $ concat $ [ show n, ". ", nick, ": [", if state then "X" else " ", "] ", P.unpack idea ]
 
--- | Add new entry to list
 addTodo :: String -> Cmd Todo ()
 addTodo rest = do
   sender <- fmap packNick getSender
   modifyMS ( ++ [ (P.pack rest, sender, False) ] )
   say "Entry added to the todo list"
 
--- | Delete an entry from the list
+updateTodo :: String -> Cmd Todo ()
+updateTodo rest = updateState rest updateCompletionStateByIndex "Syntax error. ?todo <index>, where index :: Int"
+
 delTodo :: String -> Cmd Todo ()
-delTodo rest
-  | Just n <- readM rest = say =<< withMS (\ls write -> case () of
-    _ | null ls                     -> return "Todo list is empty"
-      | n > length ls - 1 || n < 0  -> return (show n ++ " is out of range") 
-      | otherwise -> do
-        write (map snd . filter ((/= n) . fst) . zip [0..] $ ls)
-        let (a, _, _) = ls !! n
-        return ("Removed: " ++ P.unpack a))
-  | otherwise = say "Syntax error. ?todo <n>, where n :: Int"
+delTodo rest = updateState rest filterByIndex "Syntax error. ?todo <n>, where n :: Int"
+
+filterByIndex :: Int -> [IndexedEntry] -> [IndexedEntry]
+filterByIndex index = filter ((/= index) . fst)
+
+updateCompletionStateByIndex :: Int -> [IndexedEntry] -> [IndexedEntry]
+updateCompletionStateByIndex index = map $ copyOrReplace index
+
+copyOrReplace :: Int -> IndexedEntry -> IndexedEntry
+copyOrReplace index (itemNumber, (who, what, state))
+  | index == itemNumber = (itemNumber, (who, what, not state))
+  | otherwise = (itemNumber, (who, what, state))
+
+updateState :: String -> (Int -> TodoStateTransform) -> String -> Cmd Todo ()
+updateState rest transform errorMessage
+  | Just n <- readM rest = say =<< withMS (\list write -> do
+    let (updated, message) = updateItem' (transform n) n list
+    when (isJust updated) $ write $ fromJust updated
+    return message)
+  | otherwise = say errorMessage
+
+updateItem' :: TodoStateTransform -> Int -> TodoState -> TodoStateTransformResult
+updateItem' operation index list
+  | null list                         = (Nothing, "Todo list is empty")
+  | index > length list || index < 1  = (Nothing, (show index ++ " is out of range"))
+  | otherwise                         = do
+    let updated = (map snd . operation . zip [1..] $ list)
+    let (a, _, _) = list !! (index - 1)
+    (Just updated, ("Updated: " ++ P.unpack a))
