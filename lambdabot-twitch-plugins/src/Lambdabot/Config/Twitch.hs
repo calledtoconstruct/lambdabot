054aead0f3b0e4bee2fd9445f2b4895b03e906f5
Joseph Woolley <joe@calledtoconstruct.net>
2019-03-30 08:13:52 -0400

    Removing references to twitch.rc

--- lambdabot-twitch-plugins/src/Lambdabot/Config/Twitch.hs
+++ lambdabot-twitch-plugins/src/Lambdabot/Config/Twitch.hs
@@ -5,11 +5,9 @@
 {-# OPTIONS_GHC -fno-warn-overlapping-patterns -Wmissing-signatures #-}
 
 module Lambdabot.Config.Twitch (
-  reconnectDelay,
-  onStartupCmds
+  reconnectDelay
 ) where
 
 import Lambdabot.Config
 
 config "reconnectDelay" [t| Int |] [| 10000000 |]
-configWithMerge [| (++) |] "onStartupCmds" [t| [String] |] [| ["rc scripts/twitch.rc"]   |]
