2aac027e74a5410009d598bd5c27b4fc00dfefd4
Oren Rozen <countoren@gmail.com>
2019-08-29 23:27:35 -0500

    refactor default.nix and shell.nix in order to use nix-build from top level and nix-shell with vscode and hie

--- hie.nix
+++ hie.nix
@@ -1,12 +1,4 @@
-{
-    nixpkgs   ? import ./nixpkgs.nix {}
-,   version   ? "ghc864"
-,   compiler  ? import ./compiler.nix { inherit nixpkgs version; }
-}:
 let
   all-hies = import (fetchTarball "https://github.com/infinisil/all-hies/tarball/master") {};
-in
-{
-    hie = (all-hies.selection { selector = p: { inherit (p) ghc864; }; });
-}
-  
+in # Install stable HIE for GHC 8.6.5
+  (all-hies.selection { selector = p: { inherit (p) ghc865; }; })
--- shell.nix
+++ shell.nix
@@ -1,78 +1,23 @@
-{
-  nixpkgs   ? import ./nixpkgs.nix {}
-, version   ? "ghc864"
-, compiler  ? import ./compiler.nix { inherit nixpkgs version; }
-, default   ? import ./default.nix { inherit nixpkgs version withDwarf cores; }
-, hie       ? import ./hie.nix { inherit nixpkgs version compiler; }
-, vscode    ? import ./vscode.nix { inherit nixpkgs version; }
-, mkFile    ? null
-, useClang  ? false  # use Clang for C compilation
-, withLlvm  ? false
-, withDocs  ? true
-, withDwarf ? nixpkgs.stdenv.isLinux  # enable libdw unwinding support
-, withNuma  ? nixpkgs.stdenv.isLinux
-, cores     ? 16
-}:
+{ pkgs ? import ./nixpkgs.nix {} }:
+let 
+  drv = import ./.;
+  hie = import ./hie.nix;
 
-with default;
-
-let
-    env = buildEnv {
-      name = "development-environment";
-      paths = default.buildInputs ++ [ vscode.codeWrapper hie.hie nixpkgs.figlet zlib pcre pcre2 ];
-    };
-
-
-in
-stdenv.mkDerivation rec {
-  name = "vscode-with-hie-${version}";
-  buildInputs = [ env ];
-  hardeningDisable = [ "fortify" ];
-  phases = ["nobuild"];
-  postPatch = "patchShebangs .";
-  preConfigure = ''
-    echo Running preConfigure...
-    echo ${version} > VERSION
-    ((git log -1 --pretty=format:"%H") || echo dirty) > GIT_COMMIT_ID
-    ./boot
-  '' + stdenv.lib.optionalString (mkFile != null) ''
-    cp ${mkFile} mk/build.mk
-  '';
-
-  CC                  = "${stdenv.cc}/bin/cc"         ;
-  CC_STAGE0           = CC                            ;
-  CFLAGS              = "-I${env}/include"            ;
-  CPPFLAGS            = "-I${env}/include"            ;
-  LDFLAGS             = "-L${env}/lib"                ;
-  LD_LIBRARY_PATH     = "${env}/lib"                  ;
-  GMP_LIB_DIRS        = "${env}/lib"                  ;
-  GMP_INCLUDE_DIRS    = "${env}/include"              ;
-  CURSES_LIB_DIRS     = "${env}/lib"                  ;
-  CURSES_INCLUDE_DIRS = "${env}/include"              ;
-  configureFlags      = lib.concatStringsSep " "
-    ( lib.optional withDwarf "--enable-dwarf-unwind" ) ;
-
-  shellHook           = let toYesNo = b: if b then "YES" else "NO"; in ''
-    # somehow, CC gets overriden so we set it again here.
-    export CC=${stdenv.cc}/bin/cc
-
-    # "nix-shell --pure" resets LANG to POSIX, this breaks "make TAGS".
-    export LANG="en_US.UTF-8"
-
-    figlet 'Welcome to the Lambdabot Shell'
-
-  '';
-
-  enableParallelBuilding = true;
-  NIX_BUILD_CORES = cores;
-  stripDebugFlags = [ "-S" ];
-
-  # Without this, we see a whole bunch of warnings about LANG, LC_ALL and locales in general.
-  # In particular, this makes many tests fail because those warnings show up in test outputs too...
-  # The solution is from: https://github.com/NixOS/nix/issues/318#issuecomment-52986702
-  LOCALE_ARCHIVE = if stdenv.isLinux then "${glibcLocales}/lib/locale/locale-archive" else "";
-
-  nobuild = ''
-    echo Do not run this derivation with nix-build, it can only be used with nix-shell
-  '';
-}
+  hie-tools = with pkgs.haskellPackages; 
+    [ cabal-install apply-refact hie hasktags hlint hoogle brittany ]; 
+  
+  shellDrv = pkgs.haskell.lib.overrideCabal drv (drv': {
+    buildDepends =
+      (drv'.buildDepends or []) ++
+      [ (pkgs.haskell.packages.ghc865.hoogleLocal {
+          packages =
+            (drv'.libraryHaskellDepends or []) ++
+            (drv'.executableHaskellDepends or []) ++
+            (drv'.testHaskellDepends or []) ;
+        })
+        pkgs.cabal-install
+        (import ./vscode.nix { inherit pkgs; })
+      ];
+    buildTools = (drv'.buildTools or []) ++ hie-tools;
+  });
+in shellDrv.env
