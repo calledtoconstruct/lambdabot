90980c678d42db17f55ac59c223b68ddf8662442
Oren Rozen <countoren@gmail.com>
2019-09-10 23:12:22 -0500

    removing forgotten rebase comments

--- cabal-clean-all.sh
+++ cabal-clean-all.sh
@@ -1,9 +1,5 @@
 rm ./lambdabot-core/dist --recursive --force
-<<<<<<< HEAD
 rm ./lambdabot-trusted-plugins/dist --recursive --force
-=======
-rm ./lambdabot-trusted/dist --recursive --force
->>>>>>> Only delete dist folder during cleanup.
 rm ./lambdabot-hangman-plugins/dist --recursive --force
 rm ./lambdabot-points-plugins/dist --recursive --force
 rm ./lambdabot-suggest-plugins/dist --recursive --force
