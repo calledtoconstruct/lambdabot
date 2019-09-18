3c925a242f79f8d3dc47d779a6ca31e836837ece
Oren Rozen <countoren@gmail.com>
2019-09-18 12:15:54 -0500

    replace ./nixpkgs.nix with enviroment <nixpkgs> in order to avoid users of downloading entire nixpkgs.

--- nix/template.nix
+++ nix/template.nix
@@ -1,4 +1,4 @@
-(import ./nixpkgs.nix {}).writeShellScriptBin "template-haskell" ''
+(import <nixpkgs> {}).writeShellScriptBin "template-haskell" ''
   echo 'creating nix files based on the ${(import ./. {}).pname} project'
   cp -rf ${../nix} nix
   cp -f ${../default.nix} default.nix
