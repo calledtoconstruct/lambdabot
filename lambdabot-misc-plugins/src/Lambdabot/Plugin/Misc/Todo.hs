465085600e837b62064b28b0899522b90da41a4b
Joseph Woolley <joe@calledtoconstruct.net>
2021-02-28 03:08:46 +0000

    Made the todo plugin channel aware.

--- lambdabot-misc-plugins/src/Lambdabot/Plugin/Misc/Todo.hs
+++ lambdabot-misc-plugins/src/Lambdabot/Plugin/Misc/Todo.hs
@@ -6,7 +6,6 @@
 
 module Lambdabot.Plugin.Misc.Todo (todoPlugin) where
 
-import Lambdabot.Compat.PackedNick (packNick, unpackNick)
 import Lambdabot.Plugin (
   Cmd,
   LB,
@@ -20,26 +19,28 @@ import Lambdabot.Plugin (
   aliases,
   command,
   getSender,
+  getTarget,
   help,
-  modifyMS,
   moduleCmds,
   moduleDefState,
   moduleSerialize,
+  nName,
   newModule,
   privileged,
   process,
+  randomSuccessMsg,
   readM,
   readMS,
   say,
-  showNick,
   withMS,
  )
-import Lambdabot.Util (readPackedEntry)
 
 import Codec.Compression.GZip (compress, decompress)
-import Control.Monad (forM_, zipWithM)
-import qualified Data.ByteString.Char8 as P (ByteString, concat, lines, pack, unlines, unpack)
+import Control.Monad (when, zipWithM)
+import qualified Data.ByteString.Char8 as P (ByteString, concat, lines, pack, unlines, unpack, empty)
 import Data.ByteString.Lazy (fromChunks, toChunks)
+import Data.List (partition)
+import Data.Maybe (fromJust, isJust)
 
 gzip :: P.ByteString -> P.ByteString
 gzip = P.concat . toChunks . compress . fromChunks . (: [])
@@ -47,30 +48,47 @@ gzip = P.concat . toChunks . compress . fromChunks . (: [])
 gunzip :: P.ByteString -> P.ByteString
 gunzip = P.concat . toChunks . decompress . fromChunks . (: [])
 
+type ChannelName = P.ByteString
 data CompletionState = Complete | Incomplete | Deferred deriving (Eq, Show, Read)
-type AssociativeCompletionState = (P.ByteString, P.ByteString, CompletionState)
+type Todo = (P.ByteString, P.ByteString, CompletionState)
 
 instance Packable TodoState where
-  readPacked ps = readPackedEntry (splitAt 3) buildAssociativeCompletionState (P.lines . gunzip $ ps)
-  showPacked = gzip . P.unlines . concatMap (\(k, v, s) -> [k, v, P.pack . show $ s])
+  readPacked ps = consumePackedChannel [] (P.lines . gunzip $ ps)
+  showPacked = gzip . P.unlines . concatMap producePackedChannel
 
-buildAssociativeCompletionState :: [P.ByteString] -> AssociativeCompletionState
-buildAssociativeCompletionState (k : v : s : _) = (k, v, read . P.unpack $ s)
+producePackedChannel :: (ChannelName, [Todo]) -> [P.ByteString]
+producePackedChannel (thisChannelName, todos) = thisChannelName : concatMap producePackedTodo todos ++ [P.empty]
 
-completionStateAssociativeListPackedSerial :: Serial [AssociativeCompletionState]
-completionStateAssociativeListPackedSerial = Serial (Just . showPacked) (Just . readPacked)
+producePackedTodo :: Todo -> [P.ByteString]
+producePackedTodo (todo, sender, todoState) = [todo, sender, P.pack . show $ todoState]
 
-type TodoState = [AssociativeCompletionState]
-type Todo = ModuleT TodoState LB
-type IndexedEntry = (Int, AssociativeCompletionState)
+consumePackedChannel :: [(ChannelName, [Todo])] -> [P.ByteString] -> [(ChannelName, [Todo])]
+consumePackedChannel existing [] = existing
+consumePackedChannel existing (next : rest) =
+  let (todos, rest') = consumePackedTodo [] rest
+   in consumePackedChannel ((next, todos) : existing) rest'
+
+consumePackedTodo :: [Todo] -> [P.ByteString] -> ([Todo], [P.ByteString])
+consumePackedTodo existing [_] = (existing, [])
+consumePackedTodo existing ~(a : b : c : rest) = (buildTodo [a, b, c] : existing, rest)
+
+buildTodo :: [P.ByteString] -> Todo
+buildTodo ~(k : v : s : _) = (k, v, read . P.unpack $ s)
+
+serializeTodoState :: Serial TodoState
+serializeTodoState = Serial (Just . showPacked) (Just . readPacked)
+
+type TodoState = [(ChannelName, [Todo])]
+type TodoList = ModuleT TodoState LB
+type IndexedEntry = (Int, Todo)
 type TodoStateTransform = ([IndexedEntry] -> [IndexedEntry])
-type TodoStateTransformResult = (Maybe TodoState, String)
+type TodoStateTransformResult = (Maybe [Todo], String)
 
 todoPlugin :: Module TodoState
 todoPlugin =
   newModule
     { moduleDefState = return ([] :: TodoState)
-    , moduleSerialize = Just completionStateAssociativeListPackedSerial
+    , moduleSerialize = Just serializeTodoState
     , moduleCmds =
         return
           [ (command "todo")
@@ -101,36 +119,50 @@ todoPlugin =
           ]
     }
 
-getTodo :: String -> Cmd Todo ()
+getTodo :: String -> Cmd TodoList ()
 getTodo [] = readMS >>= sayTodo
 getTodo _ = say "?todo has no args, try ?add-todo or ?list todo"
 
-sayTodo :: TodoState -> Cmd Todo ()
+sayTodo :: TodoState -> Cmd TodoList ()
 sayTodo [] = say "Nothing to do!"
-sayTodo todoList =
-  mapM_ say =<< zipWithM fmtTodoItem ([1 ..] :: [Int]) todoList
+sayTodo todoList = do
+  thisChannelName <- P.pack . nName <$> getTarget
+  let thisChannelState = filter ((== thisChannelName) . fst) todoList
+  if null thisChannelState
+    then say "Nothing to do!"
+    else do
+      let thisChannel = head thisChannelState
+      mapM_ say =<< zipWithM fmtTodoItem ([1 ..] :: [Int]) (snd thisChannel)
  where
-  fmtTodoItem n (idea, nick_, state) = do
-    nick <- showNick (unpackNick nick_)
-    return $ concat [show n, ". ", nick, ": [", checkMark, "] ", P.unpack idea]
+  fmtTodoItem n (idea, nick, state) = do
+    return $ concat [show n, ". ", P.unpack nick, ": [", checkMark, "] ", P.unpack idea]
    where
     checkMark = case state of
       Complete -> "X"
       Incomplete -> " "
       Deferred -> "?"
 
-addTodo :: String -> Cmd Todo ()
-addTodo rest = do
-  sender <- fmap packNick getSender
-  modifyMS (++ [(P.pack rest, sender, Incomplete)])
-  say "Entry added to the todo list"
+addTodo :: String -> Cmd TodoList ()
+addTodo rest = withMS $ \todoState write -> do
+  sender <- P.pack . nName <$> getSender
+  thisChannelName <- P.pack . nName <$> getTarget
+  let (thisChannelState, otherChannels) = partition ((== thisChannelName) . fst) todoState
+  let thisChannel = if not (null thisChannelState) then head thisChannelState else (thisChannelName, [])
+  if any ((== P.pack rest) . getTodoMessage) $ snd thisChannel
+    then say "That item already exists."
+    else let updated = (P.pack rest, sender, Incomplete) : snd thisChannel
+     in do
+       write $ (thisChannelName, updated) : otherChannels
+       say =<< randomSuccessMsg
+    where getTodoMessage :: (a, b, c) -> a
+          getTodoMessage (a, _, _) = a
 
 toggleDeferred :: CompletionState -> CompletionState
 toggleDeferred state
   | state == Deferred = Incomplete
   | otherwise = Deferred
 
-deferTodo :: String -> Cmd Todo ()
+deferTodo :: String -> Cmd TodoList ()
 deferTodo rest = updateState rest (updateCompletionStateByIndex toggleDeferred) "Syntax error.  ?defer-todo <index>, where index :: Int"
 
 toggleComplete :: CompletionState -> CompletionState
@@ -138,10 +170,10 @@ toggleComplete state
   | state == Incomplete = Complete
   | otherwise = Complete
 
-updateTodo :: String -> Cmd Todo ()
+updateTodo :: String -> Cmd TodoList ()
 updateTodo rest = updateState rest (updateCompletionStateByIndex toggleComplete) "Syntax error. ?todo <index>, where index :: Int"
 
-delTodo :: String -> Cmd Todo ()
+delTodo :: String -> Cmd TodoList ()
 delTodo rest = updateState rest filterByIndex "Syntax error. ?todo <n>, where n :: Int"
 
 filterByIndex :: Int -> [IndexedEntry] -> [IndexedEntry]
@@ -155,19 +187,18 @@ copyOrReplace getNewState index (itemNumber, (who, what, state))
   | index == itemNumber = (itemNumber, (who, what, getNewState state))
   | otherwise = (itemNumber, (who, what, state))
 
-updateState :: String -> (Int -> TodoStateTransform) -> String -> Cmd Todo ()
+updateState :: String -> (Int -> TodoStateTransform) -> String -> Cmd TodoList ()
 updateState rest transform errorMessage
-  | Just n <- readM rest =
-    say
-      =<< withMS
-        ( \list write -> do
-            let (updated, message) = updateItem' (transform n) n list
-            forM_ updated write
-            return message
-        )
+  | Just n <- readM rest = withMS $ \todoState write -> do
+    thisChannelName <- P.pack . nName <$> getTarget
+    let (thisChannelState, otherChannels) = partition ((== thisChannelName) . fst) todoState
+    let thisChannel = if not (null thisChannelState) then head thisChannelState else (thisChannelName, [])
+    let (updated, message) = updateItem' (transform n) n $ snd thisChannel
+    when (isJust updated) $ write $ (thisChannelName, fromJust updated) : otherChannels
+    say message
   | otherwise = say errorMessage
 
-updateItem' :: TodoStateTransform -> Int -> TodoState -> TodoStateTransformResult
+updateItem' :: TodoStateTransform -> Int -> [Todo] -> TodoStateTransformResult
 updateItem' operation index list
   | null list = (Nothing, "Todo list is empty")
   | index > length list || index < 1 = (Nothing, show index ++ " is out of range")
